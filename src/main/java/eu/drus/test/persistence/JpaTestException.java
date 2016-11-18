cdd313b0b15927eb0a42642a16abd6c0ff6cf80e
Dimitrij Drus <dadrus@gmx.de>
2016-11-18 19:48:07 +0100

    removed unused constructors from JpaTestException

--- src/main/java/eu/drus/test/persistence/JpaTestException.java
+++ src/main/java/eu/drus/test/persistence/JpaTestException.java
@@ -5,24 +5,9 @@ public class JpaTestException extends RuntimeException {
     private static final long serialVersionUID = 6564897685710323054L;
 
     /**
-     * @param message
-     */
-    public JpaTestException(final String message) {
-        super(message);
-    }
-
-    /**
      * @param cause
      */
     public JpaTestException(final Throwable cause) {
         super(cause);
-    }
-
-    /**
-     * @param message
-     * @param cause
-     */
-    public JpaTestException(final String message, final Throwable cause) {
-        super(message, cause);
     }
 }
