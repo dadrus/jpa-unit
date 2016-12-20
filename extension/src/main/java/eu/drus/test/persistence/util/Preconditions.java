804244935df7e2c2921cee488c1641a102d78850
Dimitrij Drus <dadrus@gmx.de>
2016-12-20 19:13:24 +0100

    added explicit private constructors to utility classes again

--- extension/src/main/java/eu/drus/test/persistence/util/Preconditions.java
+++ extension/src/main/java/eu/drus/test/persistence/util/Preconditions.java
@@ -1,6 +1,7 @@
 package eu.drus.test.persistence.util;
 
 public final class Preconditions {
+    private Preconditions() {}
 
     public static void checkArgument(final boolean flag, final String msg) {
         if (!flag) {
--- extension/src/main/java/eu/drus/test/persistence/util/ReflectionUtils.java
+++ extension/src/main/java/eu/drus/test/persistence/util/ReflectionUtils.java
@@ -3,6 +3,7 @@ package eu.drus.test.persistence.util;
 import java.lang.reflect.Field;
 
 public final class ReflectionUtils {
+    private ReflectionUtils() {}
 
     public static void injectValue(final Field field, final Object obj, final Object value) throws IllegalAccessException {
         final boolean isAccessible = field.isAccessible();
