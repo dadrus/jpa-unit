f3743910738d7a21f3a0482954507bb4c5288940
Dimitrij Drus <dadrus@gmx.de>
2017-02-16 23:56:48 +0100

    repositories from test model fixed

--- integration-test/src/test/java/eu/drus/jpa/unit/test/model/AccountRepository.java
+++ integration-test/src/test/java/eu/drus/jpa/unit/test/model/AccountRepository.java
@@ -1,5 +1,7 @@
 package eu.drus.jpa.unit.test.model;
 
-public class AccountRepository {
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
 
-}
+@Repository
+public interface AccountRepository extends EntityRepository<Depositor, Long> {}
--- integration-test/src/test/java/eu/drus/jpa/unit/test/model/DepositorRepository.java
+++ integration-test/src/test/java/eu/drus/jpa/unit/test/model/DepositorRepository.java
@@ -1,5 +1,7 @@
 package eu.drus.jpa.unit.test.model;
 
-public class DepositorRepository {
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
 
-}
+@Repository
+public interface DepositorRepository extends EntityRepository<Depositor, Long> {}
