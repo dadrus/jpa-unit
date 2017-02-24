471087bedcdfa09cdbfb3e822577214d7106d7b0
Dimitrij Drus <dadrus@gmx.de>
2017-02-24 23:43:00 +0100

    Removed not required EntityManager instance injected by CDI

--- integration-test/src/test/java/eu/drus/jpa/unit/test/CdiBootstrappingWithJpaTest.java
+++ integration-test/src/test/java/eu/drus/jpa/unit/test/CdiBootstrappingWithJpaTest.java
@@ -1,23 +1,26 @@
 package eu.drus.jpa.unit.test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import eu.drus.jpa.unit.test.model.DepositorRepository;
+
 @RunWith(CdiTestRunner.class)
 public class CdiBootstrappingWithJpaTest {
 
     @Inject
-    private EntityManager em;
+    private DepositorRepository repository;
 
     @Test
     public void testEntityManagerIsInjected() {
-        assertNotNull(em);
-        em.clear();
+        assertNotNull(repository);
+        assertThat(repository.findAll().isEmpty(), is(Boolean.TRUE));
     }
 }
