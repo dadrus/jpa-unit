ffc492a278a9d4c8701b6bd59d7ea02ee958a813
Dimitrij Drus <dadrus@gmx.de>
2017-02-25 02:15:51 +0100

    added simple integration test showing integration of liquibase (see #2 )

---
+++ integration-test/src/test/java/eu/drus/jpa/unit/test/LiquibaseTest.java
@@ -0,0 +1,40 @@
+package eu.drus.jpa.unit.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import eu.drus.jpa.unit.api.Bootstrapping;
+import eu.drus.jpa.unit.api.JpaUnitRunner;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+@RunWith(JpaUnitRunner.class)
+public class LiquibaseTest {
+
+    @PersistenceContext(unitName = "my-verification-unit")
+    private EntityManager manager;
+
+    @Bootstrapping
+    public void prepareDataBase(final DataSource ds) throws Exception {
+        final Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(ds.getConnection()));
+        final Liquibase liquibase = new Liquibase("changelog/changelog.xml", new ClassLoaderResourceAccessor(), database);
+        liquibase.update((String) null);
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
