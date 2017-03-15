26a8e4068e9e4276aa76816a7cd82d977a29d448
Dimitrij Drus <dadrus@gmx.de>
2017-03-15 02:30:09 +0100

    moved JPA Unit extension interface to its own project

--- spi/src/main/java/eu/drus/jpa/unit/spi/TestMethodDecorator.java
+++ spi/src/main/java/eu/drus/jpa/unit/spi/TestMethodDecorator.java
@@ -4,8 +4,6 @@ public interface TestMethodDecorator {
 
     int getPriority();
 
-    // void apply(TestMethodInvocation ctx) throws Throwable;
-
     void processInstance(Object instance, TestMethodInvocation invocation) throws Exception;
 
     void beforeTest(TestMethodInvocation invocation) throws Exception;
