8252365f1abc360dd774d8da4ae0aeca97abdf54
Dimitrij Drus <dadrus@gmx.de>
2017-07-01 11:11:36 +0200

    private constructor added

--- sql/src/main/java/eu/drus/jpa/unit/sql/Constants.java
+++ sql/src/main/java/eu/drus/jpa/unit/sql/Constants.java
@@ -1,6 +1,8 @@
 package eu.drus.jpa.unit.sql;
 
-public class Constants {
+public final class Constants {
+
+    private Constants() {}
 
     public static final String KEY_DATA_SOURCE = "eu.drus.jpa.unit.sql.DataSource";
 
