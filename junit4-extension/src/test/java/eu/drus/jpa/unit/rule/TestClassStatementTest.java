cc40a7d0636f0abeb2028aa0e134c0d64f385330
Dimitrij Drus <dadrus@gmx.de>
2017-07-02 19:41:36 +0200

    tests for TestClassStatement and TestMethodStatement added

---
+++ junit4-extension/src/test/java/eu/drus/jpa/unit/rule/TestClassStatementTest.java
@@ -0,0 +1,100 @@
+package eu.drus.jpa.unit.rule;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import eu.drus.jpa.unit.spi.ExecutionContext;
+import eu.drus.jpa.unit.spi.TestClassDecorator;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestClassStatementTest {
+
+    @Mock
+    private ExecutionContext ctx;
+
+    @Mock
+    private Statement base;
+
+    @Mock
+    private TestClassDecorator decorator;
+
+    private TestClassStatement statement;
+
+    private Map<String, Object> map = new HashMap<>();
+
+    @Before
+    public void setUp() {
+        when(ctx.getData(anyString())).thenAnswer((final InvocationOnMock invocation) -> {
+            final String key = (String) invocation.getArguments()[0];
+            return map.get(key);
+        });
+
+        doAnswer((final InvocationOnMock invocation) -> {
+            final String key = (String) invocation.getArguments()[0];
+            final Object value = invocation.getArguments()[1];
+            return map.put(key, value);
+        }).when(ctx).storeData(anyString(), anyObject());
+
+        statement = new TestClassStatement(ctx, decorator, base, this);
+    }
+
+    @Test
+    public void testOnMultipleEvaluateCallsBeforeAllAndAfterAllIsCalledOnlyOnce() throws Throwable {
+        // GIVEN
+
+        // WHEN
+        statement.evaluate();
+        statement.evaluate();
+
+        // THEN
+        final InOrder inOrder = inOrder(decorator, base);
+        inOrder.verify(decorator).beforeAll(eq(ctx), eq(this.getClass()));
+        inOrder.verify(base, times(2)).evaluate();
+        inOrder.verify(decorator).afterAll(eq(ctx), eq(this.getClass()));
+    }
+
+    @Test
+    public void testBeforeAllAndAfterAllIsCalledOnlyOnceEvenInCaseOfErrors() throws Throwable {
+        // GIVEN
+        doThrow(RuntimeException.class).when(base).evaluate();
+
+        // WHEN
+        try {
+            statement.evaluate();
+            fail("Exception expected");
+        } catch (final RuntimeException e) {
+
+        }
+        try {
+            statement.evaluate();
+            fail("Exception expected");
+        } catch (final RuntimeException e) {
+
+        }
+
+        // THEN
+        final InOrder inOrder = inOrder(decorator, base);
+        inOrder.verify(decorator).beforeAll(eq(ctx), eq(this.getClass()));
+        inOrder.verify(base, times(2)).evaluate();
+        inOrder.verify(decorator).afterAll(eq(ctx), eq(this.getClass()));
+    }
+}
---
+++ junit4-extension/src/test/java/eu/drus/jpa/unit/rule/TestMethodStatementTest.java
@@ -0,0 +1,84 @@
+package eu.drus.jpa.unit.rule;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import eu.drus.jpa.unit.spi.ExecutionContext;
+import eu.drus.jpa.unit.spi.TestMethodDecorator;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestMethodStatementTest {
+
+    @Mock
+    private ExecutionContext ctx;
+
+    @Mock
+    private TestMethodDecorator decorator;
+
+    @Mock
+    private Statement base;
+
+    @Mock
+    private FrameworkMethod method;
+
+    private TestMethodStatement statement;
+
+    @Before
+    public void setUp() {
+        statement = new TestMethodStatement(ctx, decorator, base, method, this);
+        assertFalse(statement.hasErrors());
+    }
+
+    @Test
+    public void testSuccessfulStatementEvaluation() throws Throwable {
+        // GIVEN
+
+        // WHEN
+        statement.evaluate();
+
+        // THEN
+        final InOrder inOrder = inOrder(decorator, base);
+        inOrder.verify(decorator).processInstance(eq(this), eq(statement));
+        inOrder.verify(decorator).beforeTest(eq(statement));
+        inOrder.verify(base).evaluate();
+        inOrder.verify(decorator).afterTest(eq(statement));
+
+        assertFalse(statement.hasErrors());
+    }
+
+    @Test
+    public void testErroneousfulStatementEvaluation() throws Throwable {
+        // GIVEN
+        doThrow(RuntimeException.class).when(base).evaluate();
+
+        // WHEN
+        try {
+            statement.evaluate();
+            fail("Exception expected");
+        } catch (final RuntimeException e) {
+
+        }
+
+        // THEN
+        final InOrder inOrder = inOrder(decorator, base);
+        inOrder.verify(decorator).processInstance(eq(this), eq(statement));
+        inOrder.verify(decorator).beforeTest(eq(statement));
+        inOrder.verify(base).evaluate();
+        inOrder.verify(decorator).afterTest(eq(statement));
+
+        assertTrue(statement.hasErrors());
+    }
+}
