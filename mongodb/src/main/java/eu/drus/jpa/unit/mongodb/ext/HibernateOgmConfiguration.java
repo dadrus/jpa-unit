ac4b21bbe1a1e90aec93083f670bc65aaaffcdd2
Dimitrij Drus <dadrus@gmx.de>
2017-07-10 10:43:36 +0200

    mongodb support (#12) - new tests and enhancements based on findings

--- mongodb/src/main/java/eu/drus/jpa/unit/mongodb/ext/HibernateOgmConfiguration.java
+++ mongodb/src/main/java/eu/drus/jpa/unit/mongodb/ext/HibernateOgmConfiguration.java
@@ -27,6 +27,7 @@ public class HibernateOgmConfiguration extends AbstractConfiguration {
     private static final String HIBERNATE_OGM_DATASTORE_USERNAME = "hibernate.ogm.datastore.username";
     private static final String HIBERNATE_OGM_DATASTORE_HOST = "hibernate.ogm.datastore.host";
     private static final String HIBERNATE_OGM_DATASTORE_DATABASE = "hibernate.ogm.datastore.database";
+    private static final String HIBERNATE_OGM_DATASTORE_PROVIDER = "hibernate.ogm.datastore.provider";
 
     public static class ConfigurationFactoryImpl implements ConfigurationFactory {
 
@@ -34,7 +35,8 @@ public class HibernateOgmConfiguration extends AbstractConfiguration {
         public boolean isSupported(final PersistenceUnitDescriptor descriptor) {
             final Map<String, Object> dbConfig = descriptor.getProperties();
 
-            return dbConfig.containsKey(HIBERNATE_OGM_DATASTORE_DATABASE);
+            return dbConfig.containsKey(HIBERNATE_OGM_DATASTORE_PROVIDER)
+                    && ((String) dbConfig.get(HIBERNATE_OGM_DATASTORE_PROVIDER)).contains("mongo");
         }
 
         @Override
