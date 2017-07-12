077534de483438bfa0090a3cf168550a7675a326
Dimitrij Drus <dadrus@gmx.de>
2017-07-12 20:05:00 +0200

    build settings adjusted

--- build.sh
+++ build.sh
@@ -7,11 +7,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Building and analyzing a regular branch"
   
   export SONAR_ENV="-Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN"
+  export MAVEN_SETTINGS=""
   if [ "$TRAVIS_BRANCH" != "master" ]; then
     export SONAR_ENV="$SONAR_ENV -Dsonar.branch=$TRAVIS_BRANCH"
+  else
+	export MAVEN_SETTINGS="-settings .travis/settings.xml"
   fi
   
-  mvn clean verify sonar:sonar $SONAR_ENV
+  mvn $MAVEN_SETTINGS clean deploy sonar:sonar $SONAR_ENV -Dpgp.skip=false -DskipTests
 else
   echo "Building and analyzing a pull request from $TRAVIS_PULL_REQUEST_BRANCH branch"
   
