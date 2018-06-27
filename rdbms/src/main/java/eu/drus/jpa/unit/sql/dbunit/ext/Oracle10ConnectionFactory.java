c5e68216ad2fcd0e38abab0f2fdc56389af029a9
Ilkka Harmanen <ilkka.harmanen@suva.ch>
2018-06-27 15:48:15 +0200

    Connection factory always sets the schema

--- rdbms/src/main/java/eu/drus/jpa/unit/sql/dbunit/ext/Oracle10ConnectionFactory.java
+++ rdbms/src/main/java/eu/drus/jpa/unit/sql/dbunit/ext/Oracle10ConnectionFactory.java
@@ -1,12 +1,13 @@
 package eu.drus.jpa.unit.sql.dbunit.ext;
 
-import java.sql.Connection;
-
 import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.ext.oracle.Oracle10DataTypeFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Oracle10ConnectionFactory implements DbUnitConnectionFactory {
 
@@ -17,10 +18,14 @@ public class Oracle10ConnectionFactory implements DbUnitConnectionFactory {
 
     @Override
     public IDatabaseConnection createConnection(final Connection connection) throws DatabaseUnitException {
-        final IDatabaseConnection dbUnitConnection = new DatabaseConnection(connection);
-        dbUnitConnection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
-
-        return dbUnitConnection;
+        try {
+            final IDatabaseConnection dbUnitConnection = new DatabaseConnection(connection, connection.getSchema());
+            dbUnitConnection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
+        
+            return dbUnitConnection;
+        } catch (SQLException e) {
+            throw new DatabaseUnitException(e);
+        }
     }
 
 }
