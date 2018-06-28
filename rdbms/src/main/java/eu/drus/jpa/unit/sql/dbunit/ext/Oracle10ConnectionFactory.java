8ef5d3ecc47f64d5a7591b11fefec3b0a29ec1af
Ilkka Harmanen <ilkka.harmanen@suva.ch>
2018-06-28 09:27:49 +0200

    Fallback-mode to stick to old behaviour in case schema is not provided

--- rdbms/src/main/java/eu/drus/jpa/unit/sql/dbunit/ext/Oracle10ConnectionFactory.java
+++ rdbms/src/main/java/eu/drus/jpa/unit/sql/dbunit/ext/Oracle10ConnectionFactory.java
@@ -8,6 +8,7 @@ import org.dbunit.ext.oracle.Oracle10DataTypeFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class Oracle10ConnectionFactory implements DbUnitConnectionFactory {
 
@@ -18,14 +19,21 @@ public class Oracle10ConnectionFactory implements DbUnitConnectionFactory {
 
     @Override
     public IDatabaseConnection createConnection(final Connection connection) throws DatabaseUnitException {
-        try {
-            final IDatabaseConnection dbUnitConnection = new DatabaseConnection(connection, connection.getSchema());
-            dbUnitConnection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
+        Optional<String> schema = discoverSchema(connection);
+        // If no schema provided fall back to creating a connection without schema (Backwards compatibility)
+        final IDatabaseConnection dbUnitConnection
+            = schema.isPresent() ? new DatabaseConnection(connection, schema.get()) : new DatabaseConnection(connection);
         
-            return dbUnitConnection;
+        dbUnitConnection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new Oracle10DataTypeFactory());
+        return dbUnitConnection;
+    }
+    
+    private static Optional<String> discoverSchema(final Connection connection) {
+        try {
+            return Optional.of(connection.getSchema());
         } catch (SQLException e) {
-            throw new DatabaseUnitException(e);
+            return Optional.empty();
         }
     }
-
+    
 }
