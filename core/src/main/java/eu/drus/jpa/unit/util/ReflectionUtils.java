a06ea3504a0dfe29a84b8332729310dd530e40e2
Dimitrij Drus <dadrus@gmx.de>
2018-06-28 11:00:36 +0200

    ReflectionUtils enhanced to cope with inheritance while looking for fields

--- core/src/main/java/eu/drus/jpa/unit/util/ReflectionUtils.java
+++ core/src/main/java/eu/drus/jpa/unit/util/ReflectionUtils.java
@@ -5,8 +5,33 @@ import java.lang.reflect.Field;
 public final class ReflectionUtils {
     private ReflectionUtils() {}
     
+    private static Field findField(final Object src, final String fieldName) throws NoSuchFieldException {
+		Class<?> current = src.getClass();
+    	Field field = null;
+    	
+    	while(current != null && field == null) {
+    		field = getField(current, fieldName);
+    		current = current.getSuperclass();
+    	}
+    	
+    	if(field == null) {
+    		throw new NoSuchFieldException(src + " does not declare " + fieldName);
+    	}
+    	
+		return field;
+	}
+
+	private static Field getField(Class<?> clazz, final String fieldName) {
+		for (Field field : clazz.getDeclaredFields()) {
+			if(field.getName().equals(fieldName)) {
+				return field;
+			}
+		}
+		return null;
+	}
+    
     public static void injectValue(final Object obj, final String fieldName, final Object value) throws NoSuchFieldException, IllegalAccessException {
-    	injectValue(obj, obj.getClass().getDeclaredField(fieldName), value);
+    	injectValue(obj, findField(obj, fieldName), value);
     }
 
     public static void injectValue(final Object obj, final Field field, final Object value) throws IllegalAccessException {
@@ -20,7 +45,7 @@ public final class ReflectionUtils {
     }
     
     public static Object getValue(final Object src, final String fieldName) throws IllegalAccessException, NoSuchFieldException {
-    	return getValue(src, src.getClass().getDeclaredField(fieldName));
+    	return getValue(src, findField(src, fieldName));
     }
 
     public static Object getValue(final Object src, final Field field) throws IllegalAccessException {
